b743d568ab780dd7601434ffb6cf985a61ca15f1
juanchitho <jbauti9@gmail.com>
2024-03-28 12:36:48 -0300

    algo que funciona y pasa los test , faltaria hacer algunas modificaciones

--- mendez/tp0-202401mio/src/split.c
+++ mendez/tp0-202401mio/src/split.c
@@ -13,12 +13,66 @@ int contador_de_separadores(const char* string, char separador)
     }
     return contador;
 }
+char** split(const char* string, char separador)
+{
+    //si el string viene null, "no se puede separar"
+    if (string == NULL) {
+        return NULL;
+    }
+    // Armamos el char**
+    int cantidad_de_elementos_vec = contador_de_separadores(string, separador) + 1;
+    char** vec_de_punteros = (char**)malloc((size_t)(cantidad_de_elementos_vec + 1) * sizeof(char*)); // Conversión explícita de tipo
+    if (vec_de_punteros == NULL){
+        return NULL;
+    }
+    int posicion_inicial_de_palabra = 0;
+    int indice_del_vector = 0;
+    int puedo_continuar = 0;
+    int i;
 
+    for(i = 0; string[i] != '\0' && puedo_continuar != 1; i++){
+        if (string[i] == separador) {
+            int longitud_subcadena = i - posicion_inicial_de_palabra;
+            vec_de_punteros[indice_del_vector] = (char*)malloc((size_t)(longitud_subcadena + 1) * sizeof(char)); // Conversión explícita de tipo
+            if (vec_de_punteros[indice_del_vector] == NULL) {
+                puedo_continuar = 1;
+            } else {
+                strncpy(vec_de_punteros[indice_del_vector], string + posicion_inicial_de_palabra, (size_t)longitud_subcadena); // Conversión explícita de tipo
+                vec_de_punteros[indice_del_vector][longitud_subcadena] = '\0';
+                posicion_inicial_de_palabra = i + 1;
+                indice_del_vector++;
+            }
+        }
+    }
+
+    if(!puedo_continuar) {
+        size_t longitud_restante = strlen(string) - (size_t)posicion_inicial_de_palabra; // Conversión explícita de tipo
+        vec_de_punteros[indice_del_vector] = (char*)malloc((longitud_restante + 1) * sizeof(char));
+        if (vec_de_punteros[indice_del_vector] == NULL) {
+            puedo_continuar = 1;
+        } else {
+            strcpy(vec_de_punteros[indice_del_vector], string + posicion_inicial_de_palabra);
+        }
+    }
+
+    if (puedo_continuar){
+        for (int j = 0; j <= indice_del_vector; j++) {
+            free(vec_de_punteros[j]);
+        }
+        free(vec_de_punteros);
+        return NULL;
+    }
+    
+    vec_de_punteros[cantidad_de_elementos_vec] = NULL;
+    
+    return vec_de_punteros;
+}
+/*
 char** split(const char* string, char separador)
 {
     //armamos el char**
     int catidad_de_elementos_vec = contador_de_separadores(string, separador) + 1;
-    char** vec_de_punteros = (char**)malloc((catidad_de_elementos_vec + 1) * (sizeof(char*)));
+    char** vec_de_punteros = (char**)malloc((unsigned int)(catidad_de_elementos_vec + 1) * (sizeof(char*)));
     if (vec_de_punteros == NULL){
         return NULL;
     }
@@ -29,7 +83,7 @@ char** split(const char* string, char separador)
 
     for(i=0; string[i] != '\n' && puedo_continuar != 1; i++){
         if (string[i] == separador) {
-            vec_de_punteros[indice_del_vector] = malloc((i - posicion_inicial_de_palabra + 1) * (sizeof(char)));
+            vec_de_punteros[indice_del_vector] = malloc((unsigned int)(i - posicion_inicial_de_palabra + 1) * (sizeof(char)));
             if (vec_de_punteros[indice_del_vector] == NULL) {
                 puedo_continuar = 1;
             } else {
@@ -42,7 +96,8 @@ char** split(const char* string, char separador)
     }
 
     if(!puedo_continuar) {
-        vec_de_punteros[indice_del_vector] = malloc((strlen(string) - posicion_inicial_de_palabra + 1) * (sizeof(char)));
+        
+        vec_de_punteros[indice_del_vector] = (char*)malloc((strlen(string) - posicion_inicial_de_palabra + 1) * sizeof(char));
         if (vec_de_punteros[indice_del_vector] == NULL) {
             puedo_continuar = 1;
         } else {
@@ -63,3 +118,4 @@ char** split(const char* string, char separador)
     
     return vec_de_punteros;
 }
+*/
